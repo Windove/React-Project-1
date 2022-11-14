b96a5310f19d38e99d48db54a29e5b868f93bd26
Lukas <lukas@thrane.name>
2022-11-14 17:50:41 +0100

    done

--- react-project-1/src/components/Main.js
+++ react-project-1/src/components/Main.js
@@ -1,8 +1,18 @@
+// import icon
+import reactIcon from "../media/React-icon.png"
+
 // create Main component
 export default function Main() {
     return (
-        <div>
-            <h1>Main</h1>
+        <div className="main">
+            <h1>Fun facts about React</h1>
+            <ul>
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100K stars on GitHub</li>
+                <li>Is maintained by Facebook</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
         </div>
     )
 }
--- react-project-1/src/components/Navbar.js
+++ react-project-1/src/components/Navbar.js
@@ -1,8 +1,13 @@
+// import icon
+import reactIcon from "../media/React-icon.png"
+
 // create Navbar component
 export default function Navbar() {
     return (
-        <div>
-            <h1>Navbar</h1>
-        </div>
+        <nav className="nav">
+            <img src={reactIcon} className="nav_icon" alt="reactIcon" />
+            <h3>ReactFacts</h3>
+            <h4>React Course - Project 1</h4>
+        </nav>
     )
 }
